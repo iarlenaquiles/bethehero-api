8d8a818c07215aea84c4b3d65cf3395f5d2197fa
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-03-25 20:49:30 -0300

    separate controller outside routes

--- src/routes.js
+++ src/routes.js
@@ -1,31 +1,10 @@
 const express = require("express");
-const crypto = require("crypto");
-
-const connection = require("./database/connection");
+const OngController = require("./controllers/OngController");
 
 const routes = express.Router();
 
-routes.post("/ongs", async (req, res) => {
-  const { name, email, whatsapp, city, uf } = req.body;
+routes.post("/ongs", OngController.create);
 
-  const id = crypto.randomBytes(4).toString("HEX");
-
-  await connection("ongs").insert({
-    id,
-    name,
-    email,
-    whatsapp,
-    city,
-    uf
-  });
-
-  return res.json({ id });
-});
-
-routes.get("/ongs", async (req, res) => {
-  const ongs = await connection("ongs").select("*");
-
-  return res.json(ongs);
-});
+routes.get("/ongs", OngController.index);
 
 module.exports = routes;
