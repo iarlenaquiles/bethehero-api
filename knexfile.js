aba3ea21520771fe92c9a50ed846ca9fd4b95633
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-03-25 17:52:01 -0300

    create ongs migrations

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,9 @@ module.exports = {
     client: "sqlite3",
     connection: {
       filename: "./src/database/dev.sqlite"
+    },
+    migrations: {
+      directory: "./src/database/migrations"
     }
   },
 
