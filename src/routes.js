a916e74501159e701658195c5b23fb585025b0a4
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-03-25 20:40:43 -0300

    create ongs routes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,31 @@
 const express = require("express");
+const crypto = require("crypto");
+
+const connection = require("./database/connection");
 
 const routes = express.Router();
+
+routes.post("/ongs", async (req, res) => {
+  const { name, email, whatsapp, city, uf } = req.body;
+
+  const id = crypto.randomBytes(4).toString("HEX");
+
+  await connection("ongs").insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf
+  });
+
+  return res.json({ id });
+});
+
+routes.get("/ongs", async (req, res) => {
+  const ongs = await connection("ongs").select("*");
+
+  return res.json(ongs);
+});
 
 module.exports = routes;
