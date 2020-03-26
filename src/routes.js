e5522f2482a1f4e78bcefab66fd3a06686ff0d1e
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-03-25 21:35:09 -0300

    create session route

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const OngController = require("./controllers/OngController");
 const IncidentController = require("./controllers/IncidentController");
 const ProfileController = require("./controllers/ProfileController");
+const SessionController = require("./controllers/SessionController");
 
 const routes = express.Router();
 
@@ -13,5 +14,7 @@ routes.get("/incidents", IncidentController.index);
 routes.delete("/incidents/:id", IncidentController.delete);
 
 routes.get("/profile", ProfileController.index);
+
+routes.post("/sessions", SessionController.create);
 
 module.exports = routes;
