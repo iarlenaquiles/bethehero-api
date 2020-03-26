3868752fe1c3a6de321a0a6e56774e5a45d66efc
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-03-25 21:02:39 -0300

    create index incidents

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -1,7 +1,11 @@
 const connection = require("./../database/connection");
 
 module.exports = {
-  async index(req, res) {},
+  async index(req, res) {
+    const incidents = await connection("incidents").select("*");
+
+    return res.json(incidents);
+  },
   async create(req, res) {
     const { title, description, value } = req.body;
     const ong_id = req.headers.authorization;
