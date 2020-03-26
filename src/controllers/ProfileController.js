1c7cb66ecd7e411e94c44e0be199055f1423cc2e
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-03-25 21:25:17 -0300

    create profile controller

---
+++ src/controllers/ProfileController.js
@@ -0,0 +1,17 @@
+const connection = require("./../database/connection");
+
+module.exports = {
+  async index(req, res) {
+    const ong_id = req.headers.authorization;
+
+    if (!ong_id) {
+      return res.status(401).json({ error: "Not permitted" });
+    }
+
+    const incidents = await connection("incidents")
+      .where("ong_id", ong_id)
+      .select("*");
+
+    return res.json(incidents);
+  }
+};
